1a06be9e9fff782de7a272c2452d6d61b56e8474
DhruviMandaviya <mdhruvim@gmail.com>
2023-03-03 16:23:10 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI":"mongodb+srv://mdhruvim:yVNYO5l8myCytcaR@cluster0.acpi4ag.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server/routes/index.js
@@ -0,0 +1,27 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let book = require('../models/books');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+router.get('/add', (req, res, next) => {
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  res.render('books/details', {
+    title: 'Books',
+  });
+});
+
+module.exports = router;
